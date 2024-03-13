d829f64b6fd35948c58895d921e95d7a8875bb87
Caleb Grimah <lamahgrimah@gmail.com>
2024-03-13 17:33:37 +0100

    remove api key

--- packages/open_weather_api/lib/src/url_builder.dart
+++ packages/open_weather_api/lib/src/url_builder.dart
@@ -6,7 +6,7 @@ class UrlBuilder {
   final String _baseUrl;
 
   // Ideally, this api key is stored in the environment and not checked into GIT.
-  final String API_KEY = "ffd8b4178ad4b65942bec70804a53c32";
+  final String API_KEY = "";
 
   String buildGetCurrentWeatherUrl(String longitude, String latitude) {
     return '$_baseUrl/weather?lat=$latitude&lon=$longitude&appid=$API_KEY';
